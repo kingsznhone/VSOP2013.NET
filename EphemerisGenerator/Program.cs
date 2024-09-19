c4620771f065c1c17fbf50929c71ad00087f083b
ZNH <35349300+kingsznhone@users.noreply.github.com>
2024-09-19 22:30:29 +0800

    v1.3.1 (#8)

---
+++ EphemerisGenerator/Program.cs
@@ -0,0 +1,41 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+using System.Text;
+using VSOP2013;
+namespace EphemerisGenerator
+{
+    internal class Program
+    {
+        static Calculator s_calculator = new Calculator();
+
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            var enddate = new DateTime(2100, 1, 1);
+            int lineCount = 0;
+            using (StreamWriter writer = new StreamWriter("./ephemeris.csv",false))
+            {
+                writer.WriteLine("body,j2000,a,l,k,h,q,p");
+                foreach (VSOPBody body in Enum.GetValues(typeof(VSOPBody)))
+                {
+                    VSOPTime time = new VSOPTime(new DateTime(1900, 1, 1), TimeFrame.TDB);
+                    while (time.TDB < enddate)
+                    {
+                        VSOPResult_ELL result = s_calculator.GetPlanetPosition_Native(body, time);
+                        sb.Clear();
+                        sb.Append((int)body).Append(',').Append(time.J2000).Append(',');
+                        sb.Append(string.Join(", ", result.Variables_ELL));
+                        writer.WriteLine(sb.ToString());
+                        time._dt= time._dt.AddDays(1);
+                        lineCount++;
+                        if (lineCount % 1000==0)
+                        {
+                            Console.WriteLine($"Output {lineCount} lines...");
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+}
